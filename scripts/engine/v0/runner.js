62c6fc2938ee63ada38c58496211188a9fd0b654
alexandre-melgarejo <ar.melgarejo@gmail.com>
2024-11-03 05:49:34 -0400

    feat: rotinas para registro de jogadas e check de game over

--- scripts/engine/v0/runner.js
+++ scripts/engine/v0/runner.js
@@ -1,9 +1,16 @@
 
 let board = Array(9).fill(null);
 let currentPlayer = 'X'; // Começa com o jogador X
+let countX = 0;
+let countO = 0;
+let countDraw = 0;
 
 function makeMove(position) {
+
     if (!board[position]) {
+
+        let gameOver = true
+
         board[position] = currentPlayer;
         document.getElementById(`cell${position}`).textContent = currentPlayer;
 
@@ -12,19 +19,26 @@ function makeMove(position) {
                 alert(`${currentPlayer} venceu!`);
                 restartGame();
             }, 300);            
-//            restartGame();
-        } else if (board.every(cell => cell)) {
+        } else if (checkDraw()) {
             setTimeout(() => {
                 alert('Empate!');
                 restartGame();
-            }, 300);//            restartGame();
+            }, 300);
         } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            gameOver = false;
         }
+
+        showTurn();
+        if(gameOver) {
+            showScore();
+        }
+
     }
+
 }
 
-// Função para verificar se o jogador ganhou
+// Confere se a ultima jogada venceu a partida
 function checkWinner(player) {
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // linhas
@@ -32,9 +46,32 @@ function checkWinner(player) {
         [0, 4, 8], [2, 4, 6] // diagonais
     ];
 
-    return winningCombinations.some(combination => 
-        combination.every(index => board[index] === player)
-    );
+    hasWinner = winningCombinations.some(combination => 
+        combination.every(index => board[index] === player));
+
+    if(hasWinner) {
+        if(player === 'X') {
+            countX++;
+        } else {
+            countO++;
+        }
+    }
+
+    return hasWinner;
+
+}
+
+// Confere se houve empate
+function checkDraw() {
+
+    const isDraw = board.every(cell => cell);
+
+    if(isDraw){
+        countDraw++;
+    }
+
+    return isDraw;
+
 }
 
 // Função para reiniciar o jogo
@@ -44,4 +81,19 @@ function restartGame() {
     for (let i = 0; i < 9; i++) {
         document.getElementById(`cell${i}`).textContent = '';
     }
+    showScore();
+    showTurn();
+}
+
+function showScore() {
+    document.getElementById('scoreX').textContent = countX;
+    document.getElementById('scoreO').textContent = countO;
+    document.getElementById('scoreDraw').textContent = countDraw;
+    document.getElementById('scoreTotal').textContent = countX + countO + countDraw;
+}
+
+// Mostra mensagem com o jogador da vez
+function showTurn() {
+    const playerName = localStorage.getItem('player' + currentPlayer);
+    document.getElementById('message').textContent = 'Sua vez ' + playerName;
 }
